71b3152441af0dd8c6be2cdd48fd878dd723d85e
smessina <silvio.messina@eant.tech>
2021-07-06 22:01:40 -0300

    Creando prototipo de API con Flask

--- Clase 11/test.py
+++ Clase 11/test.py
@@ -1,1 +1,21 @@
-print('Hola mundo')
+from flask import Flask, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_flask():
+    return '<h1>Hola mundo</h1>'
+
+@app.route("/users")
+def twitterUsers():
+    users = [
+        { 'name' : 'smessina_' },
+        { 'name' : 'eantech' },
+        { 'name' : 'TinchoLutter' },
+        { 'name' : 'bitcoinArg' }
+    ]
+    response = app.response_class(response = json.dumps(users), status = 200, mimetype = "application/json"  )
+    
+    return response
+
+app.run( port = 3030, host = '0.0.0.0' )
