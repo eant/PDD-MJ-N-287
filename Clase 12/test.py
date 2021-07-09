f37c623a1b0ebb20fbdfa849d5e03e278046e0d2
smessina <silvio.messina@eant.tech>
2021-07-08 22:01:27 -0300

    Integracion de Python & Flask con MongoDB Atlas

--- Clase 12/test.py
+++ Clase 12/test.py
@@ -1,6 +1,19 @@
 from flask import Flask, json
+from pymongo import MongoClient
+from urllib.parse import urlencode
 
 app = Flask(__name__)
+
+###### MongoDB ######
+params = {
+    "retryWrites" : "true",
+    "w" : "majority",
+    "ssl" : "true",
+    "ssl_cert_reqs" : "CERT_NONE"
+}
+client = MongoClient("mongodb+srv://eant-python:eantpass2021@python-data-developers.osipp.mongodb.net/PDD-MJ-N-287?" + urlencode(params) )      
+db = client
+#####################
 
 @app.route("/")
 def hello_flask():
@@ -21,12 +34,53 @@ def twitterUsers():
 @app.route("/users/<path>")
 def searchUsers(path):
     
+    '''
     if path == "people":
         return "ACA VA UN JSON DE PERSONAS"
     elif path == "company":
         return "ACA VA UN JSON DE EMPRESAS"
     else:
         return "UPPS... NO PUEDO BUSCAR LO QUE ESTAS PIDIENDO :("
+    '''
+    
+    if path not in ["people", "company"]:
+        return "UPPS... NO PUEDO BUSCAR LO QUE ESTAS PIDIENDO :("
+    
+    data = db["PDD-MJ-N-287"]
+    test = data["twitter"]
+    
+    users = test.find({ "type" : path }).limit(10)
+    
+    result = []
+    
+    for user in users:
+        item = {
+            'usuario' : user['name']
+        }
+        result.append(item)
+    
+    return app.response_class(response = json.dumps(result), status = 200, mimetype = "application/json" )
+
+
+@app.route("/test")
+def test():
+    data = db["PDD-MJ-N-287"]
+    test = data["twitter"]
+    
+    
+    users = test.find()
+    
+    for user in users:
+        print(user)
+    
+    return "Mira la consola..."
+    
+
+
+
+
+
+
 
 
 app.run( port = 3030, host = '0.0.0.0' )
